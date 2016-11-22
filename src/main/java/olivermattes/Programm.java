5df9c6bbb58fe6b0007fe471ce29250be88e0723
Oliver <mattesol@hs-furtwangen.de>
2016-11-22 16:56:25 +0100

    Nachricht für 3

---
+++ src/main/java/olivermattes/Programm.java
@@ -0,0 +1,18 @@
+package olivermattes;
+import java.util.Scanner;
+public class Programm{
+
+public static void main(String[] args)
+{
+
+String eingabe ; 
+Scanner sc = new Scanner(System.in);
+System.out.println("Bitte Eingeben"); 
+eingabe = sc.next();
+System.out.println("Ausgabe: "); 
+eingabe = eingabe.toUpperCase();
+System.out.println(eingabe); 
+
+
+}
+}
